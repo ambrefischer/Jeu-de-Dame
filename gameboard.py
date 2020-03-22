844803b84d04a8b9257cc5eb8a1c209852c0ba97
amauryfischer <root@DESKTOP-A1PRRRB.localdomain>
2020-03-22 12:22:40 +0100

    better gameboard

--- gameboard.py
+++ gameboard.py
@@ -6,23 +6,24 @@ Created on Sat Mar 14 09:51:07 2020
 """
 import numpy as np
 
-    
+
 def create_gameboard():
     '''Création d'un plateau initial avec 20 pions du joueur 1
         et 20 pions du joueur 2. Les cases 0 représentent des cases vides.
         '''
-    gameboard = np.zeros((10,10))
-    for k in range(4): 
+    gameboard = np.zeros((10, 10))
+    for k in range(4):
         for j in range(10):
-            if (k+j)%2 == 1:
-                gameboard[k][j]=1
-    for k in range(6,10):
+            if (k+j) % 2 == 1:
+                gameboard[k][j] = 1
+    for k in range(6, 10):
         for j in range(10):
-            if (k+j)%2 == 1:
-                gameboard[k][j]=2
-                
+            if (k+j) % 2 == 1:
+                gameboard[k][j] = 2
+
     return gameboard
-        
-        
+
+
 def view(gameboard):
-    print("\n", gameboard)
+    for gameLine in gameboard:
+        print(gameLine)
